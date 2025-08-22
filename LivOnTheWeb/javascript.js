a5ad7bde048525d38adb51c87c41827ba803f14a
LivStokes <Oliviastokess02@gmail.com>
2025-08-22 16:47:53 +0100

    Add files via upload

--- LivOnTheWeb/javascript.js
+++ LivOnTheWeb/javascript.js
@@ -1,24 +1,36 @@
+// CONTACT BUTTON 
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("mouseover", (event) => {
+    const x = (event.pageX - btnEl.offsetLeft);
+    const y = (event.pageY - btnEl.offsetTop);
+
+    btnEl.style.setProperty("--xPos", x + "px");
+    btnEl.style.setProperty("--yPos", y + "px");
+})
 
 
-let title = document.querySelector("#title");
 
-// console.log(title.innerHTML);
-// console.log(typeof title.innerHTML);
 
-const arrayOfTitle = [...title.innerHTML];
-title.innerText = "";
-console.log(arrayOfTitle);
+// let title = document.querySelector(".title");
 
-arrayOfTitle.forEach((Char, index) => {
-    const div = document.createElement("div");
-    console.log(index[0])
-    div.style.animationDelay = 0.5 + index / 10 + "s";
-    div.innerText = Char;
-    div.className = "anime-element";
-    title.appendChile(div);
-    setTimeout(() => {
-        div.style.opacity = 1;
-    }, 1600);
+// // console.log(title.innerHTML);
+// // console.log(typeof title.innerHTML);
+
+// const arrayOfTitle = [...title.innerHTML];
+// title.innerText = "";
+// console.log(arrayOfTitle);
+
+// arrayOfTitle.forEach((Char, index) => {
+//     const div = document.createElement("div");
+//     console.log(index[0])
+//     div.style.animationDelay = 0.5 + index / 10 + "s";
+//     div.innerText = Char;
+//     div.className = "anime-element";
+//     title.appendChile(div);
+//     setTimeout(() => {
+//         div.style.opacity = 1;
+//     }, 1600);
     
-    console.log(div);
-});
+//     console.log(div);
+// });
